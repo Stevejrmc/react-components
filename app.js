f1e2304705570115dd6ac4396ff742d233fe10fd
Steve McLaughlin <stevejrmc@gmail.com>
2019-11-04 12:15:01 -0800

    Implement component sprint

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-// TODO
+// var Bread = () => (<li>Bread</li>);
+// var Milk = () => (<li>Milk</li>);
+// var GroceryListItem = props => <li>{ props.item }</li>;
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hovering: false
+    }
+  }
+
+  hovering() {
+    this.setState({
+      hovering: !this.state.hovering
+    });
+  }
+
+
+  render() {
+    var style = {
+      fontWeight: this.state.hovering ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseEnter={this.hovering.bind(this)}>{this.props.item}</li>
+    )
+  }
+}
+
+var GroceryList = props => (
+  <ul>
+    { props.items.map(item =>
+
+  <GroceryListItem item={item} key={item}/>
+
+    )}
+  </ul>
+);
+
+var App = () => (
+  <GroceryList items={['Bread', 'Milk']}/>
+);
+
+var root = document.querySelector('#app');
+ReactDOM.render(<App/>, root);
